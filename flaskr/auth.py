c05224659ba5976336d29d9b27a6fce7c7a1ca13
anu-n <anu.nanayakkara@outlook.com>
2020-08-27 06:14:42 +0000

    fixed indentation error

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -27,13 +27,13 @@ def register():
 				).fetchone() is not None:
 						error = 'User {} is already registered.'.format(username)
 
-					if error is None:
-						db.execute(
-							'INSERT INTO user (username, password) VALUES (?, ?)',
-						(username, generate_password_hash(password))
-						)
-						db.commit()
-						return redirect(url_for('auth.login'))
+				if error is None:
+					db.execute(
+						'INSERT INTO user (username, password) VALUES (?, ?)',
+					(username, generate_password_hash(password))
+					)
+					db.commit()
+					return redirect(url_for('auth.login'))
 
 				flash(error)
 
